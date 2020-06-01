338e64320c2c3b0a4346b7d9f755593abf7e2c29
justanoobcoder <syaorancode@gmail.com>
2020-06-01 09:00:09 +0700

    fix bug

--- salas.sh
+++ salas.sh
@@ -162,7 +162,7 @@ systembeepoff() {
 
 createdirs() {
     cd "/home/$name"
-    sudo -u "$name" mkdir -p user/{Downloads,Documents,Music,Videos/ScreenCaptures,Pictures/Screenshots}
+    sudo -u "$name" mkdir -p user/{Downloads,Documents,Music,Videos/ScreenCaptures,Pictures/{Wallpapers,Screenshots}}
 }
 
 xorgconfig() {
