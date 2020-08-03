f52ee8c45c48f461973263ffc204891c911651ba
justanoobcoder <syaorancode@gmail.com>
2020-08-03 12:50:37 +0700

    rewrite

--- sars.sh
+++ sars.sh
@@ -2,8 +2,7 @@
 
 ### CHECK ROOT PERMISSION ###
 
-[ $EUID -ne 0 ] && echo "Permission denied!
-Run this script as user root." && exit
+[ $EUID -ne 0 ] && echo -e "Permission denied!\nRun this script as root" && exit
 
 ### VARIABLES ###
 
@@ -13,39 +12,37 @@ aurhelper="yay"
 
 ### FUNCTIONS ###
 
-error() {
-    clear
-    printf "ERROR:\\n%s\\n" "$1"
-    exit
+error() { clear ; echo "ERROR: $1" ; exit; }
+
+pacman_install() {
+    while [ -n "$1" ]; do
+        pacman -S "$1" --noconfirm --needed >/dev/null 2>&1
+        shift
+    done
 }
 
-pacmaninstall() {
-    pacman -S "$1" --noconfirm --needed >/dev/null 2>&1
+greeting() {
+    dialog --title "SARS Installation" --yes-label "Next" --no-label "Exit" --yesno "Welcome to SARS - Syaoran's Arch Ricing Script!\\nThis script will help you install and setup a full-featured Arch linux desktop, which I use as my main machine.\\n\\nIt will overwrite all your config files, so think carefully. Choose < Exit > to exit this script or < Next > to continue." 10 80 || { clear; exit; }
 }
 
-welcomemsg() {
-    dialog --title "Welcome!" --msgbox "Welcome to SARS - Syaoran's Arch Ricing Script!\\n\\nThis script is based on Luke Smith's LARBS.\\nThis script will automatically install and setup a full-featured Arch linux desktop, which I use as my main machine." 10 60
-    dialog --title "Attention[!]" --yes-label "Next" --no-label "Exit" --yesno "This script will install and set up dwm-syaoran (my suckless's dwm build). Alsa it will overwrite all your config files, so if you don't want to continue then choose < Exit > to exit this script." 8 80 || { clear; exit; }
-}
-
-getuserandpass() {
-    username=$(dialog --inputbox "Enter a name for the user account. You can enter a user name that already exists or doesn't exist yet. It will create a new user if that user doesn't exist." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
+get_username_pw() {
+    username=$(dialog --title "SARS Installation" --inputbox "Enter username.\\nIt will create a new user if that user doesn't exist." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
     while ! echo "$username" | grep "^[a-z_][a-z0-9_-]*$" >/dev/null 2>&1; do
-        username=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _" 10 60 3>&1 1>&2 2>&3 3>&1)
+        username=$(dialog --title "SARS Installation" --no-cancel --inputbox "This username is not valid. Give username beginning with a letter, with only lowercase letters, - or _" 10 60 3>&1 1>&2 2>&3 3>&1)
     done
     id -u "$username" >/dev/null 2>&1 && user_exist="true" || {
-    pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1);
-    pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1);
+    pass1=$(dialog --title "SARS Installation" --no-cancel --insecure --passwordbox "Enter $username's password." 10 60 3>&1 1>&2 2>&3 3>&1);
+    pass2=$(dialog --title "SARS Installation" --no-cancel --insecure --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1);
     while ! [ "$pass1" = "$pass2" ]; do
         unset pass2
-        pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-        pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+        pass1=$(dialog --title "SARS Installation" --no-cancel --insecure --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+        pass2=$(dialog --title "SARS Installation" --no-cancel --insecure --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
     done ; }
 }
 
-userchoice() {
+get_option() {
     curl -Ls "$packageslist" | sed 's/  */ /g' | eval grep "\|" | sed '1,2d;s/ | /,/g;s/| //g;s/ |//g' > /tmp/temp.list
-    choice="$(dialog --title "Option" --menu "Choose one of these options:" 0 0 0 1 "Full installation (recommend)" 2 "Minimal installation" 3 "Custom installation" 3>&1 1>&2 2>&3 3>&1)"
+    choice="$(dialog --title "SARS Installation" --menu "Choose one of these options:" 0 0 0 1 "Full installation (recommend)" 2 "Minimal installation" 3 "Custom installation" 3>&1 1>&2 2>&3 3>&1)"
     [ -f /tmp/packages.list ] && rm /tmp/packages.list
     case "$choice" in
         "1")
@@ -58,13 +55,13 @@ userchoice() {
             unset n
             ;;
         "3")
-            dialog --title "Attention[!]" --msgbox "There are some packages which are selected by default. Those are important packages for SARS. DO NOT uncheck them!\nUse arrow keys to move the pointer. Press Space bar to check/uncheck package." 10 50
+            dialog --title "SARS Installation" --msgbox "There are some packages which are selected by default. Those are important packages for SARS. DO NOT uncheck them!\nUse arrow keys to move the pointer. Press Space bar to check/uncheck package." 10 50
             [ -f /tmp/options.list ] && rm /tmp/options.list
             while IFS=, read -r source program comment; do
                 n=$((n+1))
                 [ "${source#?}" = "@" ] && echo "$n $program on" >> /tmp/options.list || echo "$n $program off" >> /tmp/options.list
             done < /tmp/temp.list
-            checklist=(dialog --separate-output --checklist "Select packages that you want to install.\nChoose <Cancel> to go back." 0 0 0)
+            checklist=(dialog --title "SARS Installation" --separate-output --checklist "Select packages that you want to install.\nChoose <Cancel> to go back." 0 0 0)
             [ -z "$checklist" ] && userchoice
             options=(`cat /tmp/options.list`)
             selections=$("${checklist[@]}" "${options[@]}" 3>&1 1>&2 2>&3 3>&1)
@@ -77,32 +74,47 @@ userchoice() {
     esac
 }
 
-preinstallmsg() {
-    dialog --title "Note" --yes-label "Next" --no-label "Exit" --yesno "From now on, the installation will be automated, it won't ask for any input so you can sit back, have some coffee and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press < Next > and the system will begin installation!" 13 60 || { clear; exit; }
+last_waring() {
+    dialog --title "SARS Installation" --yes-label "Next" --no-label "Exit" --yesno "Last chance for you to quit SARS. Choose < Exit > to exit this script.\\n\\nFrom now on, the installation will be automated, it won't ask for any input so you can sit back, have some coffee and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press < Next > and the installation will begin!" 13 80 || { clear; exit; }
 }
 
-adduserandpass() {
-    dialog --infobox "Adding user \"$username\"..." 4 50
-    useradd -m -G wheel,audio,video,optical,storage -s /bin/bash "$username" >/dev/null 2>&1 ||
-    usermod -aG wheel,audio,video,optical,storage "$username" && mkdir -p /home/"$username" && chown "$username":"$username" /home/"$username"
+add_user_pw() {
+    dialog --title "SARS Installation" --infobox "Adding user \"$username\"..." 4 50
+    useradd -m -G wheel,audio,video,optical,storage,power -s /bin/bash "$username" >/dev/null 2>&1 ||
+    usermod -aG wheel,audio,video,optical,storage,power "$username" && mkdir -p /home/"$username" && chown "$username":"$username" /home/"$username"
     echo "$username:$pass1" | chpasswd
-    # Unset password variables after applying password
     unset pass1 pass2
 }
 
-refreshkeys() { \
-    dialog --infobox "Refreshing Arch Keyring..." 4 40
+refresh_keys() {
+    dialog --title "SARS Installation" --infobox "Refreshing Arch Keyring..." 4 40
     pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
 }
 
-appendsudoers() {
+install_dependencies() {
+    dialog --title "SARS Installation" --infobox "Installing \`base-devel\` and \`git\` for installing other software required for the installation of other programs." 5 70
+    pacman_install base-devel git
+}
+
+append_sudoers() {
     sed -i "/#SARS/d" /etc/sudoers
     echo "$* #SARS" >> /etc/sudoers
 }
 
+install_aurhelper() {
+    [ -f "/usr/bin/$1" ] || {
+    dialog --title "SARS Installation" --infobox "Installing \`$1\` - an AUR helper..." 4 50
+    cd /tmp || exit
+    rm -rf /tmp/"$1"
+    sudo -u "$username" git clone https://aur.archlinux.org/"$1".git >/dev/null 2>&1 &&
+    cd "$1" &&
+    sudo -u "$username" makepkg --noconfirm -si >/dev/null 2>&1
+    cd /tmp || return; }
+}
+
 maininstall() {
     dialog --title "SARS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
-    pacmaninstall "$1"
+    pacman_install "$1"
 }
 
 gitmakeinstall() {
@@ -119,7 +131,7 @@ gitmakeinstall() {
 zipmakeinstall() {
     progname="$(basename "$1")"
     dialog --title "SARS Installation" --infobox "Installing \`$progname\` ($n of $total) via a zip file from \`git\` and \`make\`. $progname $2" 5 70
-    pacmaninstall unzip
+    pacman_install unzip
     echo "$1" | grep github >/dev/null 2>&1 && \
         sudo -u "$username" curl -L "$1/archive/master.zip" -o "$repodir/${progname}.zip" >/dev/null 2>&1 || \
         sudo -u "$username" curl -L "$1/-/archive/master/${progname}-master.zip" -o "$repodir/${progname}.zip" >/dev/null 2>&1
@@ -132,18 +144,6 @@ zipmakeinstall() {
     cd /tmp || return
 }
 
-manualinstall() {
-    [ -f "/usr/bin/$1" ] || (
-    dialog --title "SARS Installation" --infobox "Installing \`$1\`, an AUR helper..." 4 50
-    cd /tmp || exit
-    rm -rf /tmp/"$1"*
-    curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
-    sudo -u "$username" tar -xvf "$1".tar.gz >/dev/null 2>&1 &&
-    cd "$1" &&
-    sudo -u "$username" makepkg --noconfirm -si >/dev/null 2>&1
-    cd /tmp || return)
-}
-
 aurinstall() {
     dialog --title "SARS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
     echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
@@ -152,11 +152,11 @@ aurinstall() {
 
 pipinstall() {
     dialog --title "SARS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
-    command -v pip || pacmaninstall python-pip >/dev/null 2>&1
+    command -v pip || pacman_install python-pip >/dev/null 2>&1
     yes | pip install "$1"
 }
 
-installationloop() {
+installation_loop() {
     total=$(wc -l < /tmp/packages.list)
     aurinstalled=$(pacman -Qqm)
     while IFS=, read -r source program comment; do
@@ -171,8 +171,19 @@ installationloop() {
     done < /tmp/packages.list
 }
 
-downloadconfig() {
-    dialog --infobox "Downloading and installing config files..." 4 60
+install_libxftbgra() {
+    dialog --title "SARS Installation" --infobox "Finally, installing \`libxft-bgra-git\` to enable color emoji in suckless softwares without crashes." 5 70
+    n=1
+    while true
+    do
+        pacman -Q libxft-bgra-git >/dev/null 2>&1 && break || yes | sudo -u "$username" $aurhelper -S libxft-bgra-git >/dev/null 2>&1
+        [ $n -eq 3 ] && error "Can't install libxft-bgra-git from AUR!"
+        n=$((n+1))
+    done
+}
+
+download_config() {
+    dialog --title "SARS Installation" --infobox "Downloading config files..." 4 40
     dir=$(mktemp -d)
     [ ! -d "$2" ] && mkdir -p "$2"
     chown -R "$username":"$username" "$dir" "$2"
@@ -183,10 +194,7 @@ downloadconfig() {
 neovim() {
     # Dependencies
     dialog --title "Neovim" --infobox "Downloading and installing dependencies..." 4 50
-    pacmaninstall neovim
-    pacmaninstall nodejs
-    pacmaninstall npm
-    pacmaninstall python-pip
+    pacman_install neovim nodejs npm python-pip
     pip3 install pynvim >/dev/null 2>&1
     sudo -u "$username" npm i -g neovim >/dev/null 2>&1
     # Plugins
@@ -199,10 +207,18 @@ neovim() {
     [ -d /home/$username/.npm ] && rm -rf /home/$username/.npm
 }
 
-systembeepoff() {
-    dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
-    rmmod pcspkr
-    echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+ch_shell_zsh() {
+    [ -d /etc/zsh ] || mkdir -p /etc/zsh
+    echo 'export ZDOTDIR="$HOME/.config/zsh"' > /etc/zsh/zshenv
+    chsh -s /bin/zsh $username >/dev/null 2>&1
+    sudo -u "$username" mkdir -p "/home/$username/.cache/zsh/"
+    rm /home/$username/.bash*
+}
+
+custom_grub() {
+    dialog --title "SARS Installation" --infobox "\nCustomizing grub..." 5 30
+    sed -i "s/#GRUB_THEME.*/GRUB_THEME=\/home\/$username\/.local\/share\/sars\/grub\/themes\/Tela\/theme.txt/g" /etc/default/grub
+    grub-mkconfig -o /boot/grub/grub.cfg >/dev/null 2>&1
 }
 
 touchpad() {
@@ -220,45 +236,41 @@ createdirs() {
     sudo -u "$username" mkdir -p .config/gtk-2.0 user/{downloads,documents,music,videos/screencast,pictures/screenshots}
 }
 
-finalmsg(){
-    dialog --title "All done!" --msgbox "Congrats! If you see this dialog then the installation gave no errors, the script completed successfully and all the programs and configuration files should be in place. Enjoy!" 12 80
+last_message(){
+    dialog --title "SARS Installation" --msgbox "Congrats! If you see this dialog then the installation gave no errors, the script completed successfully and all the programs and configuration files should be in place. Enjoy!" 8 60
 }
 
-### MAIN FUNCTION ###
-
 main() {
-    # Install dialog.
-    echo "Installing dialog package." && pacmaninstall dialog || error "Are you sure you have an internet connection?"
+    # Install dependencies
+    echo "Installing dependencies..." && pacman_install dialog curl || error "Are you sure you have an internet connection?"
 
-    # Welcome user.
-    welcomemsg || error "User exited."
+    # Greeting dialog
+    greeting
 
-    # Get and verify username and password.
-    getuserandpass || error "User exited."
+    # Get username and password
+    get_username_pw
 
-    # Get user's install option
-    userchoice || error "User exited."
+    # Get option
+    get_option
 
-    # Last chance for user to back out before install.
-    preinstallmsg || error "User exited."
+    # Last warning before the installation
+    last_waring
 
     # Add user and password if not exist.
-    [ "$user_exist" != "true" ] && { adduserandpass || error "Error adding username and/or password."; }
+    [ "$user_exist" != "true" ] && { add_user_pw || error "Can't add username and password."; }
 
-    # Create repository directory
+    # Create repo directory
     repodir="/home/$username/user/work/repo" && mkdir -p "$repodir" && chown -R "$username":"$username" $(dirname "$repodir")
 
     # Refresh Arch keyrings.
-    refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
+    refresh_keys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
-    dialog --title "SARS Installation" --infobox "Installing \`base-devel\` and \`git\` for installing other software required for the installation of other programs." 5 70
-    pacmaninstall curl
-    pacmaninstall base-devel
-    pacmaninstall git
+    # Install dependencies
+    install_dependencies || error "Can't install dependencies."
 
     # Allow user to run sudo without password. Since AUR programs must be installed
-    # in a fakeroot environment, this is required for all builds with AUR.
-    appendsudoers "%wheel ALL=(ALL) NOPASSWD: ALL"
+    #in a fakeroot environment, this is required for all builds with AUR.
+    append_sudoers "%wheel ALL=(ALL) NOPASSWD: ALL"
 
     # Make pacman and yay colorful and adds eye candy on the progress bar because why not.
     grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/Color/" /etc/pacman.conf
@@ -267,39 +279,26 @@ main() {
     # Use all cores for compilation.
     sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
-    # Install aur helper.
-    manualinstall $aurhelper || error "Failed to install AUR helper."
+    # Install AUR helper
+    install_aurhelper "$aurhelper" || error "Can't install $aurhelper."
 
     # Install all packages in packageslist.
-    installationloop
+    installation_loop
 
     # Install libxft-bgra. This is important package for suckless programs like dwm or st, they will crash without it.
-    dialog --title "SARS Installation" --infobox "Finally, installing \`libxft-bgra-git\` to enable color emoji in suckless software without crashes." 5 70
-    n=1
-    while true
-    do
-        pacman -Q libxft-bgra-git >/dev/null 2>&1 && break || yes | sudo -u "$username" $aurhelper -S libxft-bgra-git >/dev/null 2>&1
-        [ $n -eq 3 ] && error "Cannot install libxft-bgra-git from AUR!"
-        n=$((n+1))
-    done
+    install_libxftbgra
 
-    # Download dot files and put them in home directory.
-    downloadconfig "$dotfiles" "/home/$username" master
+    # Download dotfiles and put them in home directory.
+    download_config "$dotfiles" "/home/$username" master
 
     # Download and configure neovim
     neovim
 
-    # Most important command! Get rid of the beep!
-    systembeepoff
+    # Change default shell to zsh
+    ch_shell_zsh
 
-    # Make zsh the default shell for the user.
-    [ -d /etc/zsh ] || mkdir -p /etc/zsh
-    echo 'export ZDOTDIR="$HOME/.config/zsh"' > /etc/zsh/zshenv
-    chsh -s /bin/zsh $username >/dev/null 2>&1
-    sudo -u "$username" mkdir -p "/home/$username/.cache/zsh/"
-    rm /home/$username/.bash*
-
-    appendsudoers "
+    # Custom sudoers file
+    append_sudoers "
 %wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/systemctl hibernate,/usr/bin/systemctl suspend-then-hibernate,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman,/usr/bin/systemctl restart NetworkManager,/usr/bin/yay,/usr/bin/make
 Defaults editor=/usr/bin/nvim"
 
@@ -311,9 +310,7 @@ Defaults editor=/usr/bin/nvim"
     timedatectl set-local-rtc 1 --adjust-system-clock
 
     # Customize grub
-    dialog --title "SARS Installation" --infobox "\nCustomizing grub..." 5 30
-    sed -i "s/#GRUB_THEME.*/GRUB_THEME=\/home\/$username\/.local\/share\/sars\/grub\/themes\/Tela\/theme.txt/g" /etc/default/grub
-    grub-mkconfig -o /boot/grub/grub.cfg >/dev/null 2>&1
+    custom_grub || error "Can't customize grub."
 
     # Create user's directories
     createdirs
@@ -324,15 +321,17 @@ Defaults editor=/usr/bin/nvim"
     # Touchpad tap to click
     touchpad
 
+    # Remove gitrestore script if username is not syaoran
     [ "$username" != "syaoran" ] && rm /home/$username/.local/bin/gitrestore
 
     # Last message! Install complete!
-    finalmsg
-
-    # Reboot
-    dialog --title "SARS Installation" --yesno "Do you want to reboot now?" 0 0 && reboot
+    last_message
 
     clear
 }
 
+### RUN MAIN FUNCTION ###
+
 main
+
+exit 0
